9ffb0821e10d0da7d019b1ca21e117f27fa305a4
adlange <lange@iig.uni-freiburg.de>
2014-12-10 10:33:14 +0000

    - fixed bug in the visualization of start and final states of LTS

--- src/de/uni/freiburg/iig/telematik/jagal/visualization/flexible/AbstractLabeledTransitionSystemComponent.java
+++ src/de/uni/freiburg/iig/telematik/jagal/visualization/flexible/AbstractLabeledTransitionSystemComponent.java
@@ -1,11 +1,14 @@
 package de.uni.freiburg.iig.telematik.jagal.visualization.flexible;
 
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+
 import de.uni.freiburg.iig.telematik.jagal.ts.labeled.abstr.AbstractEvent;
 import de.uni.freiburg.iig.telematik.jagal.ts.labeled.abstr.AbstractLTSState;
 import de.uni.freiburg.iig.telematik.jagal.ts.labeled.abstr.AbstractLabeledTransitionRelation;
 import de.uni.freiburg.iig.telematik.jagal.ts.labeled.abstr.AbstractLabeledTransitionSystem;
 
-public class AbstractLabeledTransitionSystemComponent<G extends AbstractLabeledTransitionSystem<E,S,R,O>,E extends AbstractEvent, S extends AbstractLTSState<E, O>, R extends AbstractLabeledTransitionRelation<S, E, O>, O extends Object> extends AbstractTransitionSystemComponent<G, S, R, O> {
+public class AbstractLabeledTransitionSystemComponent<G extends AbstractLabeledTransitionSystem<E, S, R, O>, E extends AbstractEvent, S extends AbstractLTSState<E, O>, R extends AbstractLabeledTransitionRelation<S, E, O>, O extends Object>
+		extends AbstractTransitionSystemComponent<G, S, R, O> {
 
 	private static final long serialVersionUID = 4091538764969398029L;
 	private boolean useEventLabels = false;
@@ -14,22 +17,33 @@ public class AbstractLabeledTransitionSystemComponent<G extends AbstractLabeledT
 		super(ts);
 	}
 
-	
 	public boolean isUseEventLabels() {
 		return useEventLabels;
 	}
-
 
 	public void setUseEventLabels(boolean useEventLabels) {
 		this.useEventLabels = useEventLabels;
 	}
 
-
 	@Override
 	protected String getEdgeLabel(R edge) {
-		if(isUseEventLabels())
+		if (isUseEventLabels())
 			return edge.getEvent().getLabel();
 		return edge.getEvent().getName();
 	}
-	
+
+	protected void setupVisualGraph(G graph) throws Exception {
+		super.setupVisualGraph(graph);
+
+		for (S sourceVertex : graph.getStartStates()) {
+			setNodeColor(sourceNodeColor, sourceVertex.getName());
+		}
+		for (S drainVertex : graph.getEndStates()) {
+			setNodeColor(drainNodeColor, drainVertex.getName());
+		}
+
+		mxHierarchicalLayout layout = new mxHierarchicalLayout(visualGraph);
+		layout.execute(visualGraph.getDefaultParent());
+	}
+
 }
